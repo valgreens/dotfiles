b90b68fb30943b6e5abfd5c2e4c85f06814c5f80
Antonio Valverde <anvalro@gmail.com>
2014-07-28 14:29:50 +0200

    Change install script to instal oh-my-zsh if it's not installed yet

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-EEN='\e[0;32m'
+GREEN='\e[0;32m'
 RESET='\e[0m' # No Color
 PWD=`pwd`
 
@@ -6,6 +6,34 @@ function ok {
     echo -e "[${GREEN} OK ${RESET}] $1"
 }
 
+function warning {
+    echo -e "[${RED} WARNING ${RESET}] $1"
+}
+
+#
+# Install oh-my-zsh
+#
+if [ ! -d ~/.oh-my-zsh ]; then
+    git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+    ok "oh-my-zsh installed"
+else
+    warning "~/.oh-my-zsh already exists, remove it and try again!"
+fi
+
+#
+# Install vim fonts
+#
+if [ ! -d ~/.fonts ]; then
+    git clone https://github.com/Lokaltog/powerline-fonts.git ~/.fonts
+    fc-cache -vf
+    ok "vim fonts installed"
+else
+    warning "You already have some personal fonts, install the vim fonts manually: https://github.com/Lokaltog/powerline-fonts"
+fi
+
+#
+# Create links
+#
 ln -s $PWD/vimrc ~/.vimrc 
 ln -s $PWD/vim ~/.vim 
 ln -s $PWD/zshrc ~/.zshrc 
